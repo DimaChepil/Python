1dfcb5f053ea03716a29a46f9b444fe8f788f994
DimaChepil <32957296+DimaChepil@users.noreply.github.com>
2018-10-10 22:42:56 +0300

    Add files via upload

---
+++ Chepil_lab2.py
@@ -0,0 +1,9 @@
+# Створення прикладних програм на мові Python, Laboratorna 2, Dmytro Chepil, Number 15
+print('Створення прикладних програм на мові Python, Laboratorna 2')
+print('Dmytro Chepil, Number 15')
+print('Введи:')
+x = float(input('x = '))
+y = float(input('y = '))
+z = float(input('z = '))
+result = (x/(z-11.3))+(y/(z+11.3))
+print('Результат: ', result)
