83b958bd4b8f826ded9ede5269e5716dbab66bd6
DimaChepil <32957296+DimaChepil@users.noreply.github.com>
2018-10-10 22:30:02 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,11 @@
+kurs="Програмування на мові Python "
+laba="Laboratorna 1"
+name="Dmytro Chepil"
+koma=", "
+grupa="ПММ-2"
+vyklad="Savka I"
+print(kurs+laba)
+print(name+koma+grupa)
+print((vyklad+koma)*44+vyklad)
+result=(11.2/(11.2-211.3))+(1.2/(15.2+11.3))
+print("Результат :",result)
