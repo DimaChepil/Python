b68428fa43eefb721bc580af7efe3d71a1495072
DimaChepil <32957296+DimaChepil@users.noreply.github.com>
2018-10-26 00:43:56 +0300

    Update Chepil_lab8_1.py

--- Lab_8/Chepil_lab8_1.py
+++ Lab_8/Chepil_lab8_1.py
@@ -4,11 +4,11 @@
 print ('Створення прикладних програм на мові Python, Лабораторна 8_1')
 print ('Chepil Dmytro, 15')
 
-def vezhi(n, d_1, d_2, d_3) :
+def vezhi(n, a, b, c) :
   if (n == 0) : return
-  vezhi(n-1,d_1,d_3,d_2)
-  print('Кружок',n,'переміщений з', d_1, 'на' ,d_2)
-  vezhi(n-1,d_3,d_2,d_1)
+  vezhi(n-1,a,c,b)
+  print('Кружок',n,'переміщений з', a, 'на' ,b)
+  vezhi(n-1,c,b,a)
 n = int(input('Введіть N = '))
 vezhi(n,1,2,3)
 print('Переміщень N =', n, ' ==> ',2**n-1)
